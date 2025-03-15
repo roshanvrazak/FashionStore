8ca6411ba26e63dd59750e3f744d6acdb4cb273f
roshanvrazak <roshanvrazak@gmail.com>
2025-03-15 09:57:02 +0000

    Initial Commit

---
+++ src/test/java/com/fashionstore/FashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashionstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
